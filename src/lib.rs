830d0832f7d3d9405df5988a5d76e25377a11fe2
phayes <patrick.d.hayes@gmail.com>
2020-01-12 17:14:55 -0800

    Adding some basic failure tetss

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ use blake3::{Hash, Hasher};
 use growable_bloom_filter::GrowableBloom;
 use jwalk::{DirEntry, WalkDir};
 use os_str_bytes::OsStrBytes;
+use serde::{Deserialize, Serialize};
+use serde_hex::{SerHex, Strict};
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
@@ -19,12 +21,49 @@ use std::os::unix::fs::PermissionsExt;
 #[cfg(not(windows))]
 use std::os::unix::fs::FileTypeExt;
 
-pub struct Scanner {}
-
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Manifest {
-    pub digest: Hash,
+    pub path: String,
+    #[serde(with = "SerHex::<Strict>")]
+    pub digest: [u8; 32],
     pub bloom: GrowableBloom,
 }
+
+impl Manifest {
+    pub fn quick_check(&self, digest: &Hash) -> Result<(), Error> {
+        let check_hash: Hash = self.digest.into();
+        match check_hash.eq(digest) {
+            true => Ok(()),
+            false => Err(Error::ManifestCheckFail),
+        }
+    }
+
+    pub fn scan_check(&self) -> Result<(), Error> {
+        let walker = WalkDir::new(&self.path).sort(true).preload_metadata(true);
+        let mut hasher = blake3::Hasher::new();
+        for entry in walker {
+            let entry: jwalk::DirEntry = entry?;
+            let filename = &entry.file_name.to_string_lossy().into_owned();
+
+            hash_entry(entry, &mut hasher)?;
+
+            let checkpoint = hasher.clone().finalize();
+
+            if !self.bloom.contains(&checkpoint) {
+                dbg!("Error on file: {}", filename);
+                return Err(Error::ManifestCheckFail);
+            }
+        }
+
+        let digest = hasher.finalize();
+
+        if !&digest.eq(&self.digest) {}
+
+        Ok(())
+    }
+}
+
+pub struct Scanner {}
 
 impl Scanner {
     pub fn new() -> Self {
@@ -36,19 +75,21 @@ impl Scanner {
         let mut hasher = blake3::Hasher::new();
         for entry in walker {
             let entry: jwalk::DirEntry = entry?;
-            self.hash_entry(entry, &mut hasher)?;
+            hash_entry(entry, &mut hasher)?;
         }
 
         Ok(hasher.finalize())
     }
 
     pub fn build_manifest<P: AsRef<Path>>(&self, root: P) -> Result<Manifest, Error> {
-        let walker = WalkDir::new(root).sort(true).preload_metadata(true);
+        let walker = WalkDir::new(root.as_ref())
+            .sort(true)
+            .preload_metadata(true);
         let mut hasher = blake3::Hasher::new();
         let mut checkpoints = Vec::new();
         for entry in walker {
             let entry: jwalk::DirEntry = entry?;
-            self.hash_entry(entry, &mut hasher)?;
+            hash_entry(entry, &mut hasher)?;
 
             checkpoints.push(hasher.clone().finalize());
         }
@@ -60,134 +101,109 @@ impl Scanner {
 
         let digest = hasher.finalize();
 
-        Ok(Manifest { digest, bloom })
+        Ok(Manifest {
+            path: format!("{}", root.as_ref().display()),
+            digest: digest.as_bytes().to_owned(),
+            bloom,
+        })
     }
+}
 
-    pub fn check_manifest<P: AsRef<Path>>(
-        &self,
-        root: P,
-        manifest: &Manifest,
-    ) -> Result<(), Error> {
-        let walker = WalkDir::new(root).sort(true).preload_metadata(true);
-        let mut hasher = blake3::Hasher::new();
-        for entry in walker {
-            let entry: jwalk::DirEntry = entry?;
-            let filename = &entry.file_name.to_string_lossy().into_owned();
+// TODO: filename in error
+fn hash_entry(entry: DirEntry, hasher: &mut Hasher) -> Result<(), Error> {
+    // Destructure the the entry
+    let DirEntry {
+        depth: _,
+        file_type: _,
+        content_spec: _,
+        parent_spec,
+        content_error: _,
+        metadata,
+        file_name,
+    } = entry;
 
-            self.hash_entry(entry, &mut hasher)?;
+    // Construct the full path to the entry
+    let path: PathBuf = parent_spec.path.join(&file_name);
 
-            let checkpoint = hasher.clone().finalize();
+    // Unwrap the metadata
+    let metadata = metadata
+        .expect("Cannot fetch file metadata")
+        .map_err(|e| Error::EntryErr(e, format!("{}", &path.display())))?;
 
-            if !manifest.bloom.contains(&checkpoint) {
-                dbg!("Error on file: {}", filename);
-            }
-        }
+    // Update the hash as per the filetype
+    let filetype = metadata.file_type();
+    if filetype.is_dir() {
+        hasher.update(&[0]);
+    } else if filetype.is_symlink() {
+        hasher.update(&[1]);
+    } else if filetype.is_file() {
+        hasher.update(&[2]);
+    } else {
+        // It's something else we don't know about
+        #[cfg(windows)]
+        hasher.update(&[255]);
 
-        let digest = hasher.finalize();
-
-        if !&digest.eq(&manifest.digest) {}
-
-        Ok(())
-    }
-
-    // TODO: filename in error
-    fn hash_entry(&self, entry: DirEntry, hasher: &mut Hasher) -> Result<(), Error> {
-        // Destructure the the entry
-        let DirEntry {
-            depth: _,
-            file_type: _,
-            content_spec: _,
-            parent_spec,
-            content_error: _,
-            metadata,
-            file_name,
-        } = entry;
-
-        // Construct the full path to the entry
-        let path: PathBuf = parent_spec.path.join(&file_name);
-
-        // Unwrap the metadata
-        let metadata = metadata
-            .expect("Cannot fetch file metadata")
-            .map_err(|e| Error::EntryErr(e, format!("{}", &path.display())))?;
-
-        // Update the hash as per the filetype
-        let filetype = metadata.file_type();
-        if filetype.is_dir() {
-            hasher.update(&[0]);
-        } else if filetype.is_symlink() {
-            hasher.update(&[1]);
-        } else if filetype.is_file() {
-            hasher.update(&[2]);
-        } else {
-            // If we're not on windows hash the device type
-            #[cfg(not(windows))]
-            {
-                if filetype.is_block_device() {
-                    hasher.update(&[3]);
-                } else if filetype.is_char_device() {
-                    hasher.update(&[4]);
-                } else if filetype.is_fifo() {
-                    hasher.update(&[5]);
-                } else if filetype.is_socket() {
-                    hasher.update(&[6]);
-                } else {
-                    // It's something else we don't know about
-                    // TODO: return error??
-                    hasher.update(&[255]);
-                }
-            }
-
-            // It's something else we don't know about
-            // TODO: return error??
-            #[cfg(windows)]
-            hasher.update(&[255]);
-        }
-
-        // Update the hash as per file permissions
-        let permissions = metadata.permissions();
-        if permissions.readonly() {
-            hasher.update(&[0]);
-        } else {
-            hasher.update(&[1]);
-        }
-
-        // If we're not on windows looks an the entire mode permissions
+        // If we're not on windows hash the device type
         #[cfg(not(windows))]
         {
-            use std::mem::transmute;
-            let permissions = metadata.permissions();
-            let bytes: [u8; 4] = unsafe { transmute(permissions.mode().to_be()) };
-            hasher.update(&bytes);
+            if filetype.is_block_device() {
+                hasher.update(&[3]);
+            } else if filetype.is_char_device() {
+                hasher.update(&[4]);
+            } else if filetype.is_fifo() {
+                hasher.update(&[5]);
+            } else if filetype.is_socket() {
+                hasher.update(&[6]);
+            } else {
+                // It's something else we don't know about
+                hasher.update(&[255]);
+            }
         }
-
-        // If it's a file or a symlink, hash it's contents
-        if filetype.is_file() {
-            let file = File::open(&path)
-                .map_err(|e| Error::EntryErr(e, format!("{}", &path.display())))?;
-            let mut buf_reader = BufReader::new(file);
-
-            let buffer = buf_reader
-                .fill_buf()
-                .map_err(|e| Error::EntryErr(e, format!("{}", &path.display())))?;
-            hasher.update(&buffer);
-            let len = buffer.len();
-            buf_reader.consume(len);
-        }
-
-        // If it's a symlink, hash it's target
-        if filetype.is_symlink() {
-            let link = std::fs::read_link(&path)
-                .map_err(|e| Error::EntryErr(e, format!("{}", &path.display())))?;
-            hasher.update(link.as_os_str().to_bytes().as_ref());
-        }
-
-        // Finally update the hash with the filename
-        let os_path = path.into_os_string();
-        hasher.update(&os_path.to_bytes());
-
-        Ok(())
     }
+
+    // Update the hash as per file permissions
+    let permissions = metadata.permissions();
+    if permissions.readonly() {
+        hasher.update(&[0]);
+    } else {
+        hasher.update(&[1]);
+    }
+
+    // If we're not on windows looks an the entire mode permissions
+    #[cfg(not(windows))]
+    {
+        use std::mem::transmute;
+        let permissions = metadata.permissions();
+        let bytes: [u8; 4] = unsafe { transmute(permissions.mode().to_be()) };
+        hasher.update(&bytes);
+    }
+
+    // If it's a file or a symlink, hash it's contents
+    if filetype.is_file() {
+        let file =
+            File::open(&path).map_err(|e| Error::EntryErr(e, format!("{}", &path.display())))?;
+        let mut buf_reader = BufReader::new(file);
+
+        let buffer = buf_reader
+            .fill_buf()
+            .map_err(|e| Error::EntryErr(e, format!("{}", &path.display())))?;
+        hasher.update(&buffer);
+        let len = buffer.len();
+        buf_reader.consume(len);
+    }
+
+    // If it's a symlink, hash it's target
+    if filetype.is_symlink() {
+        let link = std::fs::read_link(&path)
+            .map_err(|e| Error::EntryErr(e, format!("{}", &path.display())))?;
+        hasher.update(link.as_os_str().to_bytes().as_ref());
+    }
+
+    // Finally update the hash with the filename
+    let os_path = path.into_os_string();
+    hasher.update(&os_path.to_bytes());
+
+    Ok(())
 }
 
 #[derive(Debug, Fail)]
@@ -197,6 +213,9 @@ pub enum Error {
 
     #[fail(display = "{}: io error: {}", 1, 0)]
     EntryErr(std::io::Error, String),
+
+    #[fail(display = "Manifest file integrity check failed - something has changed")]
+    ManifestCheckFail,
 }
 
 impl From<std::io::Error> for Error {
@@ -208,11 +227,43 @@ impl From<std::io::Error> for Error {
 #[cfg(test)]
 mod tests {
     use crate::*;
-    #[test]
-    fn it_works() {
-        let scanner = Scanner::new();
-        let hash = scanner.scan("/opt/opticalgym").unwrap();
 
-        dbg!(hash);
+    #[test]
+    fn basic_test() {
+        let scanner = Scanner::new();
+        let manifest = scanner.build_manifest("./test").unwrap();
+
+        let hash = scanner.scan("./test").unwrap();
+
+        manifest.quick_check(&hash).unwrap();
+        manifest.scan_check().unwrap();
+    }
+
+    #[test]
+    fn failure_test() {
+        // Copy `test` to a temporary directory that we can modify
+        let temp_dir = "./temp_test";
+
+        copy_dir::copy_dir("./test", temp_dir).unwrap();
+
+        //  Make sure we clean up
+        let _guard = scopeguard::guard((), |_| fs_extra::dir::remove(temp_dir).unwrap());
+
+        // Create the scanner and the manifest
+        let scanner = Scanner::new();
+        let manifest = scanner.build_manifest(temp_dir).unwrap();
+
+        // So far so good - nothig has changed
+        let hash = scanner.scan(temp_dir).unwrap();
+        manifest.quick_check(&hash).unwrap();
+        manifest.scan_check().unwrap();
+
+        // Modify a file
+        fs_extra::file::write_all("./temp_test/README.md", "   EXTRA DATA").unwrap();
+        let hash = scanner.scan(temp_dir).unwrap();
+
+        // Assert that both the quick_check and the full scan-check fails
+        assert!(manifest.quick_check(&hash).is_err());
+        assert!(manifest.scan_check().is_err());
     }
 }
